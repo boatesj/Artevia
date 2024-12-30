7e71de1b53c20ea96b5e0d0bde36787b165cf194
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2024-12-30 17:35:56 +0000

    Install Pillow and create initial migrations for models

--- artworks/models.py
+++ artworks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Artwork(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='artworks')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='artworks/')
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
